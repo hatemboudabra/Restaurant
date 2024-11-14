60b056e42e234fa3aec0dec978e30a94f75f0623
mouhamedjouini <mouhadje@gmail.com>
2024-11-14 20:16:17 +0100

    payement-fixed

---
+++ restaurant/src/main/java/com/livrini/restaurant/dto/PaymentRequest.java
@@ -0,0 +1,13 @@
+package com.livrini.restaurant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequest {
+    private String productName;
+    private float amount;
+}
