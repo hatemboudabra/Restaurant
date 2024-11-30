365b89aca6a3f794680b7245ab5abf714cc4c739
hatem <hatemboudabra41@gmail.com>
2024-11-30 19:56:49 +0100

    correct dast

--- restaurant/main.tf
+++ restaurant/main.tf
@@ -1,14 +1,19 @@
 provider "kubernetes" {
   host                   = "https://192.168.58.2:8443"
   token                  = var.k8s_token
+  config_path            = "~/.kube/config"
   cluster_ca_certificate = file("./ca.crt")
 }
+
 variable "k8s_token" {
   type = string
 }
+
+# Secret PostgreSQL
 resource "kubernetes_secret" "postgres_secret" {
   metadata {
-    name = "postgres-secret"
+    name      = "postgres-secret"
+    namespace = "default"
   }
 
   data = {
@@ -23,7 +28,8 @@ resource "kubernetes_secret" "postgres_secret" {
 
 resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
   metadata {
-    name = "postgres-pvc"
+    name      = "postgres-pvc"
+    namespace = "default"
   }
 
   spec {
@@ -40,9 +46,11 @@ resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
   }
 }
 
+# Déploiement Backend
 resource "kubernetes_deployment" "backend" {
   metadata {
-    name = "backend"
+    name      = "backend"
+    namespace = "default"
   }
 
   spec {
